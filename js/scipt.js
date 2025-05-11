94b589db5162af39addee4a2371a8e88b1ea6996
Margo0107 <megi.busenka@gmail.com>
2025-05-11 22:16:59 +0300

    completed task 11.1 pythagorean table

---
+++ js/scipt.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('table-container');
+const table = document.createElement('table');
+
+for (let row = 1; row <= 10; row++){
+    const tr = document.createElement('tr');
+
+    for (let col = 1; col <= 10; col++) {
+    const td = document.createElement('td');
+    td.textContent = row * col;
+    tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+container.appendChild(table);
