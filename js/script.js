51722971ce9d5dca6df5635af6eb0dda6c52e27f
Margo0107 <megi.busenka@gmail.com>
2025-05-13 04:37:33 +0300

    task 11.3 completed, when you click on the button a random picture appears

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
-const container = document.getElementById('table-container');
-const table = document.createElement('table');
+//Pythagorean table
+const container = document.getElementById("table-container");
+const table = document.createElement("table");
 
-for (let row = 1; row <= 10; row++){
-    const tr = document.createElement('tr');
+for (let row = 1; row <= 10; row++) {
+  const tr = document.createElement("tr");
 
-    for (let col = 1; col <= 10; col++) {
-    const td = document.createElement('td');
+  for (let col = 1; col <= 10; col++) {
+    const td = document.createElement("td");
     td.textContent = row * col;
     tr.appendChild(td);
-    }
-    table.appendChild(tr);
+  }
+  table.appendChild(tr);
 }
 container.appendChild(table);
 
+//the text changes color when you click on the button
+const text = document.getElementById("text");
+const button = document.getElementById("toggleBtn");
 
+button.addEventListener("click", () => {
+  text.classList.toggle("red");
+});
 
-const text = document.getElementById('text');
-const button = document.getElementById('toggleBtn');
+//random image + btn random
+const image = document.getElementById("randomImage");
+const buttonImg = document.getElementById("randomBtn");
 
-button.addEventListener('click', () => {
-    text.classList.toggle('red');
+buttonImg.addEventListener("click", () => {
+  const randomNumbers = Math.floor(Math.random() * 9) + 1;
+
+  const imagePath = `image/${randomNumbers}_img.jpg`;
+  image.src = imagePath;
 });
